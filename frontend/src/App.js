dabad5beca6cdc094d842730fd1d7177832a0f66
Ajay Kumar Gupta <gajay3954@gmail.com>
2022-01-07 10:04:46 +0530

    frontend: added routing using react-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,20 @@
-// import logo from './logo.svg'
-import './App.css'
-// import HomePage from './screens/HomePage'
-// import ProfileCard from './components/ProfileCard'
-import Profile from './screens/Profile'
-// import HomePage from './screens/HomePage'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import HomePage from './screens/HomePage'
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" exact element={<HomePage />} />
+    </Routes>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <Profile />
+      <Router>
+        <AppRoutes />
+      </Router>
     </div>
   )
 }
