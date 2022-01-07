20227d2b9041eaacdd747b621186163bd011a7a9
Ajay Kumar Gupta <gajay3954@gmail.com>
2022-01-07 11:23:16 +0530

    frontend: added console logging styles

---
+++ frontend/src/utils/logger.js
@@ -0,0 +1,9 @@
+const warnStyles = 'font-weight:bold;'
+const infoStyles = 'font-weight:bold; color:green'
+const errorStyles = 'font-weight:bold; color:red'
+const headStyles =
+  'color: white; font-size:15px; background-color: #666666; width: 100%'
+const subheadStyles =
+  'color: white; color: #666666; background-color: #F2F2F2; width: 100%'
+
+export { warnStyles, infoStyles, errorStyles, headStyles, subheadStyles }
