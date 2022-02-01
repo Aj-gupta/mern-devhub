8f08b0b13a1a4f00a67575ecac4bb2c024ee4248
Ajay Kumar Gupta <gajay3954@gmail.com>
2022-02-01 18:05:08 +0530

    backend: added urlencoded extended true
    to support nested objects in request body

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,7 @@ const io = new Server(server, {
 
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
 if (process.env.NODE_ENV === 'development') {
