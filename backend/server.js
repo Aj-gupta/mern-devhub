bb1af26a7291eadb8216d0a67a48c094741c1341
Ajay Kumar Gupta <gajay3954@gmail.com>
2022-02-01 21:48:49 +0530

    changes for live branch

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import morgan from 'morgan'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 
+import helmet from 'helmet'
 import connectDB from './config/db.js'
 import { addUser, removeUser } from './utils/users.js'
 
@@ -21,14 +22,22 @@ connectDB()
 
 const app = express()
 const server = http.createServer(app)
+app.use(helmet())
+app.use(helmet.hidePoweredBy())
+
+app.use(
+  cors({
+    origin: 'devhub-dev.netlify.app',
+    optionsSuccessStatus: 200,
+  })
+)
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: 'devhub-dev.netlify.app',
     methods: ['GET', 'POST'],
   },
 })
 
-app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
